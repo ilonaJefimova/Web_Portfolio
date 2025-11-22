550af2100e16a8cbe2282624cad051f38510da68
ilonaJefimova <74294928+ilonaJefimova@users.noreply.github.com>
2025-11-22 00:25:35 +0000

    Update Models

--- Block4/model/model_create.php
+++ Block4/model/model_create.php
@@ -1,25 +1,26 @@
 <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">
   <div class="modal-dialog">
-    <form class="modal-content shadow" method="POST" action="../controller/insert_news.php">
-      <div class="modal-header bg-dark text-white">
+    <form class="modal-content shadow ounded-3" method="POST" action="../controller/insert_news.php">
+      <div class="modal-header bg-dark text-white  rounded-top">
         <h5 class="modal-title" id="createModalLabel">Create News</h5>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
 
-      <div class="modal-body">
-        <label class="mt-2">Title</label>
+      <div class="modal-body bg-light">
+        <label class="mt-2 d-block">Title</label>
         <input type="text" class="form-control" name="title" required>
 
-        <label class="mt-2">Description</label>
+        <label class="mt-2 d-block">Description</label>
         <input type="text" class="form-control" name="description" required>
 
-        <label class="mt-2">Link</label>
+        <label class="mt-2 d-block">Link</label>
         <input type="text" class="form-control" name="link" required>
 
-        <input type="hidden" name="image" value="coming_soon.png">
+        <label class="mt-2 d-block">Image (optional)</label>
+        <input type="file" class="form-control" name="image">
       </div>
 
-      <div class="modal-footer bg-dark">
+      <div class="modal-footer bg-dark rounded-bottom">
         <button type="submit" class="btn btn-success">Create</button>
       </div>
     </form>
--- Block4/model/model_update.php
+++ Block4/model/model_update.php
@@ -1,34 +1,36 @@
 <?php foreach ($items as $n): ?>
     <div class="modal fade" id="updateModal<?= $n->id ?>" aria-labelledby="updateModalLabel" aria-hidden="true">
         <div class="modal-dialog">
-            <form class="modal-content shadow" method="POST" action="../controller/update_news.php">
+            <form class="modal-content shadow ounded-3" method="POST" action="../controller/update_news.php">
 
-                <div class="modal-header bg-dark text-white">
+                <div class="modal-header bg-dark text-white rounded-top">
                     <h5 class="modal-title" id="updateModalLabel">Update News</h5>
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
 
-                <div class="modal-body">
+                <div class="modal-body bg-light">
 
                     <input type="hidden" name="id" value="<?= $n->id ?>">
 
-                    <label class="mt-2">Title</label>
+                    <label class="mt-2 d-block">Title</label>
                     <input type="text" class="form-control" name="title" value="<?= $n->title ?>">
 
-                    <label class="mt-2">Description</label>
+                    <label class="mt-2 d-block">Description</label>
                     <input type="text" class="form-control" name="description" value="<?= $n->description ?>">
 
-                    <label class="mt-2">Link</label>
+                    <label class="mt-2 d-block">Link</label>
                     <input type="text" class="form-control" name="link" value="<?= $n->link ?>">
 
-                    <label class="mt-2">Image (current)</label><br>
-                    <img src="../image/<?= $n->image ?>" class="rounded mb-2" style="height:80px;">
+                    <label class="mt-2 d-block">Current Image</label>
+                    <input type="hidden" name="current_image" value="<?= $n->image ?>">
+                    <img src="../image/<?= $n->image ?>" class="img-fluid rounded mb-2" style="height:80px;">
 
-                    <input type="hidden" name="image" value="<?= $n->image ?>">
+                    <label class="mt-2 d-block">Upload New Image (optional)</label>
+                    <input type="file" class="form-control" name="image">
 
                 </div>
 
-                <div class="modal-footer bg-dark">
+                <div class="modal-footer bg-dark rounded-bottom">
                     <button type="submit" class="btn btn-primary">Save Changes</button>
                 </div>
 
